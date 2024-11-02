9a71e6a9649d33f27f6c2684d2351f32c698d367
Thiago Walek <146485866+ThiagoWalek@users.noreply.github.com>
2024-11-02 09:20:37 -0300

    Projeto a principio finalizado

--- projetos-python/projeto-4-bi/projeto2.py
+++ projetos-python/projeto-4-bi/projeto2.py
@@ -1,5 +1,15 @@
 import pandas as pd
 import openpyxl as  pxl
+import os
+import time
+from prettytable import PrettyTable
+
+def limpar_tela():
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
 def verificar_situacao(media):
     if(media >= 6):
         situacao = "APROVADO"
@@ -14,7 +24,6 @@ def verificar_situacao(media):
         
 def gerar_exel():
     global lista_dados
-    global i
     dados_para_excel = []
     for aluno in lista_dados:
         dados_para_excel.append({
@@ -27,19 +36,21 @@ def gerar_exel():
             "Média": aluno["Média"]
         })
 
-    # def para_planilha():
     df = pd.DataFrame(dados_para_excel)
     print(df)
     df.to_excel('NOTASFINAISALUNOS.xlsx', sheet_name = 'planilha_alunos', na_rep = '#N/A', header = True, index = False)
 
-def verifica_notas(nome_materia, nota):
-    if(str(nota).isnumeric == True):
-        while nota < 0 or nota > 10:
-            nota = float(input(f"Nota inválida! Redigite a nota de {nome_materia} (entre 0 e 10): "))
-    else:
-        while nota < 0 or nota > 10 and str(nota).isnumeric != True:
-            nota = float(input(f"Nota inválida! Redigite a nota de {nome_materia} (entre 0 e 10): ")) 
-    return nota
+def verifica_notas(nome_materia):
+    while True:
+        try:
+            nota = float(input(f"Digite a nota de {nome_materia} (entre 0 e 10): "))
+            if 0 <= nota <= 10:
+                return nota
+            else:
+                print("Nota inválida! A nota deve estar entre 0 e 10.")
+        except ValueError:
+            nota = input("Nota inválida! Por favor, insira um valor numérico: ")
+
 lista_dados = []
 def forms_digitacao():
     global i
@@ -53,14 +64,10 @@ def forms_digitacao():
             while(nome_aluno == "" or nome_aluno.isnumeric() == True or len(nome_aluno) <= 3):
                 nome_aluno = input("Redigite o nome do aluno: ")
 
-            nota_pvb = float(input("Digite a nota da matéria P.V.B.: "))
-            verifica_notas("P.V.B.",nota_pvb)
-            nota_paw = float(input("Digite a nota da matéria P.A.W.: "))
-            verifica_notas("P.A.W.",nota_paw)
-            nota_bd = float(input("Digite a nota da matéria B.D.: "))
-            verifica_notas("B.D.",nota_bd)
-            nota_poo = float(input("Digite a nota da matéria P.O.O.I.: "))
-            verifica_notas("P.O.O.I.",nota_poo)
+            nota_pvb = verifica_notas("P.V.B.")
+            nota_paw = verifica_notas("P.A.W.")
+            nota_bd = verifica_notas("B.D.")
+            nota_poo = verifica_notas("P.O.O.I.")
             media = (nota_pvb + nota_paw + nota_poo + nota_bd) / 4
             situacao, cor = verificar_situacao(media)
             lista_dados.append({"Matrícula":aluno_ra, "Nome":nome_aluno, "Nota de PVB":nota_pvb, "Nota de PAW":nota_paw, "Nota de BD":nota_bd, "Nota de POOI":nota_poo, "Média":media, "Situação":situacao, "Cor":cor})
@@ -75,6 +82,7 @@ def forms_digitacao():
     except Exception as erro:
         print("Ocorreu um erro: ",erro)
         menu()
+    limpar_tela()
     menu()
 def gerar_html(lista_dados):
     for item in lista_dados:
@@ -82,22 +90,29 @@ def gerar_html(lista_dados):
         arquivo.write(f"<!DOCTYPE html>\n<html lang='pt-br'>\n<head>\n    <meta charset='UTF-8'>\n    <meta name='viewport' content='width=device-width, initial-scale=1.0'>\n    <title>{item['Matrícula']}</title>\n<link rel='stylesheet' href='style.css'>\n</head>\n<body>\n<div>\n    <p>Matricula: {item['Matrícula']}</p>\n    <p>Aluno</p>\n    <p id='nome'>{item['Nome']}<p>\n    <table>\n        <tr>\n            <th>PVB</th>\n            <th>PAW</th>\n            <th>BD</th>\n            <th>POO</th>\n            <th>MEDIA</th>\n        </tr>\n        <tr>\n            <td>{item['Nota de PVB']:,.1f}</td>\n            <td>{item['Nota de PAW']:,.1f}</td>\n            <td>{item['Nota de BD']:,.1f}</td>\n            <td>{item['Nota de POOI']:,.1f}</td>\n            <td>{item['Média']:,.1f}</td>\n        </tr>\n    </table>\n    <p>Situacao final: <p style='background-color: {item['Cor']};'>{item['Situação']}</p></p>\n</div>\n</body>\n</html>")
         arquivo.close()
 
-def ver():
-    pass
+def visualizar_exel():
+    df = pd.read_excel("NOTASFINAISALUNOS.xlsx")
+    tabela = PrettyTable()
+    tabela.field_names = df.columns.tolist()
+    for _, row in df.iterrows():
+        tabela.add_row(row.tolist())
+    print(tabela)
 
 def menu():
-    print("1 - Inserir dados exel de aluno(s);")
+    print("1 - Inserir dados de aluno(s);")
     print("2 - Ver alunos do exel;")
     print("3 - Sair.")
-    opc = int(input("Escolha uma das opções: "))
-    while(opc < 1 or opc > 4):
-        opc = int(input("Escolha uma das opções entre 1 e 4 apenas: "))
+    opc = int(input("Escolha uma das opções entre  1 e 3: "))
+
+    while(opc < 1 or opc > 3):
+        opc = int(input("Escolha uma das opções entre 1 e 3 apenas: "))
     if(opc == 1):
         forms_digitacao()
     elif(opc == 2):
-        ver()
+        visualizar_exel()
     elif(opc == 3):
         print("Saindo...")
+        time.sleep(1.4) 
 menu()
 print("FIM DO Programa!!!")
 # def gerar_html():
